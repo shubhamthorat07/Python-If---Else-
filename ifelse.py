103a4945f18e560d069e81111c77a3acc5c46f98
Shubham <40167163+shubhamthorat07@users.noreply.github.com>
2020-07-29 16:10:31 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,23 @@
+num1 = 200
+num2 = 300
+
+if num1>num2:
+	print("num1 is greater than num2")
+
+else:
+	print("num1 is less than num2")
+
+
+
+num3 = 500
+num4 = 1000
+
+if num3>num4:
+   print("num3 is greater than num4")
+
+
+elif num4>num3:
+	print("num4 is greater than num4")
+
+else:
+	print("num3 and num4 are equal")
